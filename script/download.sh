4e1200f0ca573bad6b7680d98c448bbdace09d26
Paulo Alencar <pbalencar@gmail.com>
2012-12-03 00:47:01 -0300

    Adicionado o script download.sh e o arquivo list

---
+++ script/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+while read line
+do
+    echo "Baixando http://ichart.finance.yahoo.com/table.csv?s=$line&d=11&e=3&f=2012&g=d&a=0&b=3&c=2000&ignore=.csv"
+    wget -O "../data/$line" "http://ichart.finance.yahoo.com/table.csv?s=$line&d=11&e=3&f=2012&g=d&a=0&b=3&c=2000&ignore=.csv"
+done < list
