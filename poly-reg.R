ec7a71e5231ec2d13340cf8fb062ba83a8b0be73
Paulo Alencar <pbalencar@gmail.com>
2012-12-12 00:44:47 -0300

    Parâmetros no script de regressão.

--- poly-reg.R
+++ poly-reg.R
@@ -1,38 +1,47 @@
-require(quantmod)
+"polyRegression" <- function (Symbol) 
+{
+  require(quantmod)
 
-getSymbols("CPFE3.SA", src="yahoo")
+  #getSymbols("Symbol", src="yahoo")
 
-x <- as.integer(index(CPFE3.SA))
-y <- as.double((Hi(CPFE3.SA)+Lo(CPFE3.SA)+Cl(CPFE3.SA))/3)
+  x <- as.integer(index(Symbol))
+  y <- as.double((Hi(Symbol)+Lo(Symbol)+Cl(Symbol))/3)
 
-o = order(x)
+  o = order(x)
 
-#do.it <- function (model, col) {
-#  r <- lm( model )
-#  yp <- predict(r)
-#  lines( yp[o] ~ x[o], col=col, lwd=3 )
-#}
-#
-#plot( y~x )
-#
-#x <- as.integer(index(CPFE3.SA))
-#
-#do.it(y~poly(x,2), col="blue")
+  #do.it <- function (model, col) {
+  #  r <- lm( model )
+  #  yp <- predict(r)
+  #  lines( yp[o] ~ x[o], col=col, lwd=3 )
+  #}
+  #
+  #plot( y~x )
+  #
+  #x <- as.integer(index(Symbol))
+  #
+  #do.it(y~poly(x,2), col="blue")
 
-x <- as.Date(index(CPFE3.SA))
+  x <- as.Date(index(Symbol))
 
-#plot( y~x )
+  #plot( y~x )
 
-x <- as.integer(index(CPFE3.SA))
+  x <- as.integer(index(Symbol))
 
-r <- lm(y~poly(x,2))
+  r <- lm(y~poly(x,2))
 
-yp <- predict(r)
+  yp <- predict(r)
 
-yr <- xts(yp, as.Date(x))
+  yr <- xts(yp, as.Date(x))
 
-chartSeries(CPFE3.SA,
-  TA=c(addTA(yr,on=1,col=3),
-  addTA(yr+(summary(r)$sigma),on=1,col=7),
-  regm <- addTA(yr-(summary(r)$sigma),on=1,col=7))
-)
+  chartSeries(Symbol,
+	      TA=c(addTA(yr,on=1,col=3),
+		  addTA(yr+(summary(r)$sigma),on=1,col=7),
+		  regm <- addTA(yr-(summary(r)$sigma),on=1,col=7))
+  )
+
+  z <- x*0
+  z[y > (yp+(summary(r)$sigma))] <- +1
+  z[y < (yp-(summary(r)$sigma))] <- -1
+  z[y > (yp+((summary(r)$sigma)*2))] <- +2
+  z[y < (yp-((summary(r)$sigma)*2))] <- -2
+}
