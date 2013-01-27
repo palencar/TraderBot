acb818cf209ed856d86fc166b89fdc58a5fedd66
Paulo Alencar <pbalencar@gmail.com>
2013-01-27 14:39:26 -0300

    Adicionado script de desenho dos gráficos a partir de objetos pre-computados.

---
+++ plot_objects.R
@@ -0,0 +1,17 @@
+source("startProbe.R")
+source("filters.R")
+source("poly-reg.R")
+source("plot.R")
+
+if(exists(Symbols) == FALSE)
+{
+  Symbols <- startProbe()
+}
+
+args_cmd <- commandArgs(trailingOnly=TRUE)
+
+for(name in args_cmd)
+{
+  #print(name)
+  plotObject(name, dev="png")
+}
