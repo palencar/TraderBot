e6c260a2d00a23b44def9fb78d2b374318d2631a
Paulo Alencar <pbalencar@gmail.com>
2013-06-02 21:44:45 -0400

    - Regressão otimizada

--- poly-reg.R
+++ poly-reg.R
@@ -1,3 +1,10 @@
+library('RcppEigen')
+library('multicore')
+library(foreach)
+library(doMC)
+#library('LargeRegression')
+
+
 polyRegression <- function (Symbol)
 {
   require(quantmod)
@@ -18,22 +25,27 @@ polyRegression <- function (Symbol)
   
   x <- as.Date(index(Symbol))
   
-  r <- lm(y~poly(x,2))
+  #r <- lm(y~poly(x,2))
+  #r <- fastLm(y, as.matrix(poly(x,2)))
+  #r <- fastLmPure(as.matrix(poly(x,2)), y)
+  r <- RcppEigen::fastLmPure(poly(x,2), y)
   
-  yp <- predict(r)
+  #yp <- predict(r)
+  yp <- predict.mlm(r)+mean(r$residuals)
   
-  lastDay <- as.Date(xts::last(index(Symbol)))
-  next10Day <- as.Date(lastDay + 10)
-  dataextra<-data.frame(x=seq(lastDay,next10Day,1))
+  #lastDay <- as.Date(xts::last(index(Symbol)))
+  #next10Day <- as.Date(lastDay + 10)
+  #dataextra<-data.frame(x=seq(lastDay,next10Day,1))
   
-  ep <- predict(lm(y~poly(x,2)),dataextra) 
+  #ep <- predict(lm(y~poly(x,2)),dataextra) 
   
   yr <- xts(yp, as.Date(x))
   
   diffReg <- diff(yr)
   diffVal <- xts(y-yp, as.Date(x))
   
-  return(list(regression=yr, diffReg=diffReg, diffVal=diffVal, sigma=summary(r)$sigma))
+  #return(list(regression=yr, diffReg=diffReg, diffVal=diffVal, sigma=summary(r)$sigma))
+  return(list(regression=yr, diffReg=diffReg, diffVal=diffVal, sigma=sd(r$residuals)))
 }
 
 findCurves <- function(SymbolName, minDays, maxDays, dateLimit="")
@@ -42,6 +54,7 @@ findCurves <- function(SymbolName, minDays, maxDays, dateLimit="")
   k <- 1
   
   for(i in minDays:maxDays)
+  #foreach (i = minDays:maxDays) %dopar%
   {
     if(dateLimit == "")
     {    
@@ -77,8 +90,8 @@ findCurves <- function(SymbolName, minDays, maxDays, dateLimit="")
     lista[[k]]$trend    <- dtrend
     lista[[k]]$period   <- i
     
-    result <- sprintf("%s %s %d", lista[[k]]$name, lista[[k]]$trend, lista[[k]]$period)
-    print(result)
+    #result <- sprintf("%s %s %d", lista[[k]]$name, lista[[k]]$trend, lista[[k]]$period)
+    #print(result)
     
     k <- k+1
   }
@@ -110,7 +123,7 @@ findBestCurve <- function(SymbolName, minDays, maxDays, dateLimit="")
       dateInterval <- sprintf("%s::%s", ds[2], ds[1])
     }
     
-    lista <- polyRegression(get(SymbolName)[dateInterval])
+    lista <- polyRegression(get(SymbolName)[dateInterval])    
     if(lista$sigma < minSigmaValue)
     {
       minSigmaPeriod <- i
@@ -233,27 +246,30 @@ processRegressions <- function(Symbols, StartDate, EndDate)
   k1 <- 10
   k2 <- 730
   
+  registerDoMC()
+  
   for(dt in seq(as.Date(EndDate), as.Date(StartDate), by = "-1 day"))
   {
     chartDate <- sprintf("%s", as.Date(dt))
     
     filterSymbols <- filterIncomplete(Symbols)
-    
-    for(symbol in filterSymbols)
+
+    foreach (symbol = filterSymbols) %dopar%
+    #for(symbol in filterSymbols)
     {
-      if(length(get(symbol)[chartDate]) == 0)
-        next
-      
-      strOut <- sprintf("findCurves %s %d %d %s", symbol, k1, k2, chartDate)
-      print(strOut)
-      
-      alertas <- findCurves(symbol, k1, k2, dateLimit=chartDate)
-      
-      if(length(alertas) > 0)
+      if(length(get(symbol)[chartDate]) > 0)
       {
-        objectName <- sprintf("backtest/%s-%s_%d_%d.rds", chartDate, symbol, k1, k2)
+        strOut <- sprintf("findCurves %s %d %d %s", symbol, k1, k2, chartDate)
+        print(strOut)
         
-        saveRDS(alertas, file=objectName)
+        alertas <- findCurves(symbol, k1, k2, dateLimit=chartDate)
+        
+        if(length(alertas) > 0)
+        {
+          objectName <- sprintf("backtest/%s-%s_%d_%d.rds", chartDate, symbol, k1, k2)
+          
+          saveRDS(alertas, file=objectName)
+        }
       }
     }
   }
