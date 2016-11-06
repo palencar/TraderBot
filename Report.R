23a419978be3312dc7c85adde46fc68f1084fd36
Paulo Alencar <pbalencar@gmail.com>
2016-11-06 20:37:44 -0300

    - Relatório um pouco mais útil.

--- Report.R
+++ Report.R
@@ -1,5 +1,4 @@
 library("data.table")
-#library("quantmod")
 
 args <- commandArgs(trailingOnly=TRUE)
 
@@ -23,45 +22,102 @@ for(file in files)
 
 dataTable <- rbindlist(data)
   
-png("reportX1.png")
-smaPeriod <- dataTable#[!duplicated(dataTable[,c('V1', 'V11')]),]
-scatter.smooth(smaPeriod$V1, smaPeriod$V11, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
-dev.off()
+smaPeriod <- data.frame(dataTable$V1, dataTable$V11)
+smaPeriod <- smaPeriod[complete.cases(smaPeriod),]
+if(nrow(smaPeriod) > 0)
+{
+  png("smaPeriod.png")
+  colnames(smaPeriod) <- c("smaPeriod", "proffit")
+  scatter.smooth(smaPeriod, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
+  dev.off()
+}
 
-png("reportX2.png")
-upperBand <- dataTable#[!duplicated(dataTable[,c('V2', 'V11')]),]
-scatter.smooth(upperBand$V2, upperBand$V11, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
-dev.off()
+upperBand <- data.frame(dataTable$V2, dataTable$V11)
+upperBand <- upperBand[complete.cases(upperBand),]
+if(nrow(upperBand) > 0)
+{
+  png("upperBand.png")
+  colnames(upperBand) <- c("upperBand", "proffit")
+  scatter.smooth(upperBand, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
+  dev.off()
+}
 
-png("reportX3.png")
-lowerBand <- dataTable#[!duplicated(dataTable[,c('V3', 'V11')]),]
-scatter.smooth(lowerBand$V3, lowerBand$V11, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
-dev.off()
+lowerBand <- data.frame(dataTable$V3, dataTable$V11)
+lowerBand <- lowerBand[complete.cases(lowerBand),]
+if(nrow(lowerBand) > 0)
+{
+  png("lowerBand.png")
+  colnames(lowerBand) <- c("lowerBand", "proffit")
+  scatter.smooth(lowerBand, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
+  dev.off()
+}
 
-png("reportX4.png")
-downChange <- dataTable#[!duplicated(dataTable[,c('V4', 'V11')]),]
-scatter.smooth(downChange$V4, downChange$V11, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
-dev.off()
+downChange <- data.frame(dataTable$V4, dataTable$V11)
+downChange <- downChange[complete.cases(downChange),]
+if(nrow(downChange) > 0)
+{
+  png("downChange.png")
+  colnames(downChange) <- c("downChange", "proffit")
+  scatter.smooth(downChange, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
+  dev.off()
+}
 
-png("reportX5.png")
-upChange <- dataTable#[!duplicated(dataTable[,c('V5', 'V11')]),]
-scatter.smooth(upChange$V5, upChange$V11, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
-dev.off()
+upChange <- data.frame(dataTable$V5, dataTable$V11)
+upChange <- upChange[complete.cases(upChange),]
+if(nrow(upChange) > 0)
+{
+  png("upChange.png")
+  colnames(upChange) <- c("upChange", "proffit")
+  scatter.smooth(upChange, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
+  dev.off()
+}
 
-png("reportX6.png")
-lowerLimit <- dataTable#[!duplicated(dataTable[,c('V6', 'V11')]),]
-scatter.smooth(lowerLimit$V6, lowerLimit$V11, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
-dev.off()
+lowerLimit <- data.frame(dataTable$V6, dataTable$V11)
+lowerLimit <- lowerLimit[complete.cases(lowerLimit),]
+if(nrow(lowerLimit) > 0)
+{
+  png("lowerLimit.png")
+  colnames(lowerLimit) <- c("lowerLimit", "proffit")
+  scatter.smooth(lowerLimit, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
+  dev.off()
+}
 
-png("reportX7.png")
-stopGain <- dataTable#[!duplicated(dataTable[,c('V7', 'V11')]),]
-scatter.smooth(stopGain$V7, stopGain$V11, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
-dev.off()
+stopGain <- data.frame(dataTable$V7, dataTable$V11)
+stopGain <- stopGain[complete.cases(stopGain),]
+if(nrow(stopGain) > 0)
+{
+  png("stopGain.png")
+  colnames(stopGain) <- c("stopGain", "proffit")
+  scatter.smooth(stopGain, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
+  dev.off()
+}
 
-png("reportX8.png")
-stopLoss <- dataTable#[!duplicated(dataTable[,c('V8', 'V11')]),]
-scatter.smooth(stopLoss$V8, stopLoss$V11, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
-dev.off()
+stopLoss <- data.frame(dataTable$V2, dataTable$V11)
+stopLoss <- stopLoss[complete.cases(stopLoss),]
+if(nrow(stopLoss) > 0)
+{
+  png("stopLoss.png")
+  colnames(stopLoss) <- c("stopLoss", "proffit")
+  scatter.smooth(stopLoss, col=rgb(0,100,0,50,maxColorValue=255), pch=16)
+  dev.off()
+}
+
+symbols <- dataTable$symbol
+symbols <- symbols[!duplicated(symbols)]
+profList <- list()
+
+i <- 1
+for(symbol in symbols)
+{
+  index <- which(dataTable$symbol == symbol)
+  proffit <- mean(dataTable[index]$V11)
+  profList[[i]] <- data.frame(symbol, proffit)
+  i <- i + 1
+}
+
+meanProffit <- rbindlist(profList)
+proffitOrder <- meanProffit[with(meanProffit, order(proffit)), ]
+print(proffitOrder)
 
 #dff <- NULL
 #data <- list()
