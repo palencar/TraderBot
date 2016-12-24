0f581846dea187c5e0fb70718ffdc0990b790e77
Paulo Alencar <pbalencar@gmail.com>
2016-12-24 18:27:27 -0300

    - Chart tab.

--- server.R
+++ server.R
@@ -17,35 +17,72 @@ shinyServer(function(input, output)
 {
   symbol_env <- new.env()
 
-  make_chart <- function(symbol) {
+  make_chart <- function(symbol, startDate, endDate) {
     symbol_data <- require_symbol(symbol, symbol_env)
 
     if(!is.null(symbol))
-      chartSymbols(Symbols=symbol, startDate = input$daterange[1], endDate = input$daterange[2],
-                   timeFrame = input$time_frame)
+      chartSymbols(Symbols=symbol, startDate = startDate, endDate = endDate,
+                   timeFrame = input$alertsTimeFrame)
   }
 
   observe({
     alerts <- getAlerts(input$numAlerts)
     symbols <- unique(as.vector(alerts$symbol))
-    numCharts <- min(length(symbols), input$numAlerts)
+    numAlerts <- min(length(symbols), input$numAlerts)
 
-    for (i in 1:numCharts) {
-      local({
-        my_i <- i
-        plotname <- paste("plot", my_i, sep="")
-        output[[plotname]] <- renderPlot({ make_chart(symbols[[my_i]]) })
-      })
+    if(numAlerts > 0)
+    {
+      for(i in 1:numAlerts)
+      {
+        local({
+          my_i <- i
+          startDate <- input$alertsDateRange[1]
+          endDate   <- input$alertsDateRange[2]
+
+          plotname <- paste("alerts", my_i, sep="")
+          output[[plotname]] <- renderPlot({ make_chart(symbols[[my_i]], startDate, endDate) })
+        })
+      }
+    }
+
+    numCharts <- length(input$symbolNames)
+
+    if(numCharts > 0)
+    {
+      for(i in 1:numCharts)
+      {
+        local({
+          my_i <- i
+          my_symbol <- input$symbolNames[[my_i]]
+          startDate <- input$chartsDateRange[1]
+          endDate   <- input$chartsDateRange[2]
+
+          plotname <- paste("charts", my_i, sep="")
+          output[[plotname]] <- renderPlot({ make_chart(my_symbol, startDate, endDate) })
+        })
+      }
     }
   })
 
-  output$charts <- renderUI({
+  output$alerts <- renderUI({
     plot_output_list <- lapply(1:input$numAlerts, function(i) {
-      plotname <- paste("plot", i, sep="")
+      plotname <- paste("alerts", i, sep="")
       plotOutput(plotname)
     })
 
     do.call(tagList, plot_output_list)
+  })
+
+  output$charts <- renderUI({
+    if(length(input$symbolNames) > 0)
+    {
+      plot_output_list <- lapply(1:length(input$symbolNames), function(i) {
+        plotname <- paste("charts", i, sep="")
+        plotOutput(plotname)
+      })
+
+      do.call(tagList, plot_output_list)
+    }
   })
 
   output$parameters     <- renderPlot({par(mfrow=c(2,4))
--- ui.R
+++ ui.R
@@ -1,28 +1,56 @@
 library("shiny")
+library("TraderBot")
 
 shinyUI(navbarPage("TraderBot",
-                    tabPanel("Backtest",
-                               plotOutput("parameters", height = "800px"),
-                               dataTableOutput("dataTable")
-                             ),
+                   tabPanel("Backtest",
+                            plotOutput("parameters", height = "800px"),
+                            dataTableOutput("dataTable")),
 
-                    tabPanel("Alerts",
-                             sidebarPanel(
-                               headerPanel("Parameters"),
+                   tabPanel("Alerts",
+                            sidebarPanel(
+                              headerPanel("Options"),
 
-                               numericInput("numAlerts", "Number of alerts to view:", 5, min = 0, max = 30),
+                              numericInput("numAlerts", "Number of alerts to view:", 5, min = 0, max = 30),
 
-                               selectInput(inputId = "time_frame",
-                                           label = "Time frame",
-                                           choices = c("Daily" = "daily",
-                                                       "Weekly" = "weekly")),
+                              selectInput(inputId = "alertsTimeFrame",
+                                          label = "Time frame",
+                                          choices = c("Daily" = "daily",
+                                                      "Weekly" = "weekly")),
 
-                               dateRangeInput(inputId = "daterange", label = "Date range",
-                                              start = Sys.Date() - 730, end = Sys.Date())
-                               ),
+                              radioButtons("alertsPeriod", "Period",
+                                           c("6m"=180, "1y"=365, "2y"=730, "5y"=1825, "10y"=3650), selected = 730),
 
-                             mainPanel(
-                               uiOutput("charts")
-                             )
-                   )
+                              dateRangeInput(inputId = "alertsDateRange", label = "Date range",
+                                             start = Sys.Date() - 730, end = Sys.Date())
+                              ),
+
+                            mainPanel(
+                              uiOutput("alerts")
+                              )
+                            ),
+
+                   tabPanel("Charts",
+                            sidebarPanel(
+                              headerPanel("Options"),
+
+                              selectizeInput(
+                                'symbolNames', 'Symbols', choices = getSymbolNames(), multiple = TRUE
+                              ),
+
+                              selectInput(inputId = "chartsTimeFrame",
+                                          label = "Time frame",
+                                          choices = c("Daily" = "daily",
+                                                      "Weekly" = "weekly")),
+
+                              radioButtons("chartsPeriod", "Period",
+                                           c("6m"=180, "1y"=365, "2y"=730, "5y"=1825, "10y"=3650), selected = 730),
+
+                              dateRangeInput(inputId = "chartsDateRange", label = "Date range",
+                                             start = Sys.Date() - 730, end = Sys.Date())
+                              ),
+
+                            mainPanel(
+                              uiOutput("charts")
+                            )
+                            )
 ))
