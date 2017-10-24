c269e189936347baa86db8e2ec81cb29a64b2d0f
Paulo Alencar <pbalencar@gmail.com>
2017-10-24 14:42:21 -0300

    - Update schema files

--- mysql-schema.sql
+++ mysql-schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
 --
--- Host: localhost    Database: traderbot
+-- Host: traderbot.############.#########.rds.amazonaws.com    Database: traderbot
 -- ------------------------------------------------------
--- Server version	5.7.18-0ubuntu0.17.04.1
+-- Server version	5.6.35-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,38 +16,20 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `db`
+-- Table structure for table `alerts`
 --
 
-DROP TABLE IF EXISTS `db`;
+DROP TABLE IF EXISTS `alerts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `db` (
-  `Host` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `Db` char(64) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `User` char(16) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `Select_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Insert_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Update_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Delete_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Drop_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Grant_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `References_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Index_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Alter_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_tmp_table_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Lock_tables_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_view_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Show_view_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_routine_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Alter_routine_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Execute_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Event_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Trigger_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  PRIMARY KEY (`Host`,`Db`,`User`),
-  KEY `User` (`User`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Database privileges';
+CREATE TABLE `alerts` (
+  `symbol` varchar(12) DEFAULT NULL,
+  `timeframe` varchar(3) DEFAULT NULL,
+  `datetime` datetime DEFAULT NULL,
+  `alert` varchar(4) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  UNIQUE KEY `intradayindex` (`symbol`,`timeframe`,`datetime`,`alert`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,14 +51,6 @@ CREATE TABLE `intraday` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `alerts` (
-  `symbol` varchar(12) DEFAULT NULL,
-  `timeframe` varchar(3) DEFAULT NULL,
-  `datetime` datetime DEFAULT NULL,
-  `alert` varchar(4) DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  UNIQUE KEY `intradayindex` (`symbol`,`timeframe`,`datetime`,`alert`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 --
 -- Table structure for table `operations`
 --
@@ -91,29 +65,6 @@ CREATE TABLE `operations` (
   `size` int(11) DEFAULT NULL,
   `price` float DEFAULT NULL,
   `cost` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `stockinfo`
---
-
-DROP TABLE IF EXISTS `stockinfo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `stockinfo` (
-  `symbol` varchar(12) NOT NULL DEFAULT '',
-  `name` varchar(64) NOT NULL DEFAULT '',
-  `exchange` varchar(16) NOT NULL DEFAULT '',
-  `capitalisation` float DEFAULT NULL,
-  `low_52weeks` float DEFAULT NULL,
-  `high_52weeks` float DEFAULT NULL,
-  `earnings` float DEFAULT NULL,
-  `dividend` float DEFAULT NULL,
-  `p_e_ratio` float DEFAULT NULL,
-  `avg_volume` int(11) DEFAULT NULL,
-  `active` tinyint(1) DEFAULT '1',
-  PRIMARY KEY (`symbol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -139,184 +90,6 @@ CREATE TABLE `stockprices` (
   UNIQUE KEY `stockindex` (`symbol`,`date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `stockprices_intraday`
---
-
-DROP TABLE IF EXISTS `stockprices_intraday`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `stockprices_intraday` (
-  `symbol` varchar(12) NOT NULL DEFAULT '',
-  `datetime` datetime DEFAULT NULL,
-  `previous_close` float DEFAULT NULL,
-  `min_open` float DEFAULT NULL,
-  `min_low` float DEFAULT NULL,
-  `min_high` float DEFAULT NULL,
-  `min_close` float DEFAULT NULL,
-  `bid` float DEFAULT NULL,
-  `ask` float DEFAULT NULL,
-  `volume` int(11) DEFAULT NULL,
-  KEY `stockprices_intraday_stockprices_pkey` (`symbol`,`datetime`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `Host` char(60) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `User` char(16) COLLATE utf8_bin NOT NULL DEFAULT '',
-  `Password` char(41) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  `Select_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Insert_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Update_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Delete_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Drop_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Reload_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Shutdown_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Process_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `File_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Grant_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `References_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Index_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Alter_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Show_db_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Super_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_tmp_table_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Lock_tables_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Execute_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Repl_slave_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Repl_client_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_view_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Show_view_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_routine_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Alter_routine_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_user_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Event_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Trigger_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `Create_tablespace_priv` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  `ssl_type` enum('','ANY','X509','SPECIFIED') CHARACTER SET utf8 NOT NULL DEFAULT '',
-  `ssl_cipher` blob NOT NULL,
-  `x509_issuer` blob NOT NULL,
-  `x509_subject` blob NOT NULL,
-  `max_questions` int(11) unsigned NOT NULL DEFAULT '0',
-  `max_updates` int(11) unsigned NOT NULL DEFAULT '0',
-  `max_connections` int(11) unsigned NOT NULL DEFAULT '0',
-  `max_user_connections` int(11) unsigned NOT NULL DEFAULT '0',
-  `plugin` char(64) COLLATE utf8_bin DEFAULT 'mysql_native_password',
-  `authentication_string` text COLLATE utf8_bin,
-  `password_expired` enum('N','Y') CHARACTER SET utf8 NOT NULL DEFAULT 'N',
-  PRIMARY KEY (`Host`,`User`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Users and global privileges';
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = latin1 */ ;
-/*!50003 SET character_set_results = latin1 */ ;
-/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`rdsadmin`@`localhost`*/ /*!50003 trigger block_user_i before insert on user
-for each row
-BEGIN
-  DECLARE foo varchar(255);
-  if new.User = "rdsadmin" then
-    select `ERROR (RDS): CANNOT CREATE RDSADMIN USER` into foo from mysql.user;
-  end if;
-
-  if new.User = "rdsrepladmin" then
-    select `ERROR (RDS): CANNOT CREATE RDSREPLADMIN USER` into foo from mysql.user;
-  end if;
-
-   if new.super_priv = 'Y' then
-     select `ERROR (RDS): SUPER PRIVILEGE CANNOT BE GRANTED OR MAINTAINED` into foo from mysql.user;
-   elseif new.shutdown_priv = 'Y' then
-     select `ERROR (RDS): SHUTDOWN PRIVILEGE CANNOT BE GRANTED OR MAINTAINED. PLEASE USE RDS API` into foo from mysql.user;
-   elseif new.file_priv = 'Y' then
-     select `ERROR (RDS): FILE PRIVILEGE CANNOT BE GRANTED OR MAINTAINED` into foo from mysql.user;
-   elseif new.repl_slave_priv = 'Y' then
-     select `ERROR (RDS): REPLICA SLAVE PRIVILEGE CANNOT BE GRANTED OR MAINTAINED` into foo from mysql.user;
-   elseif new.repl_client_priv = 'Y' then
-       select `ERROR (RDS): REPLICA CLIENT PRIVILEGE CANNOT BE GRANTED OR MAINTAINED` into foo from mysql.user;
-  end if;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = latin1 */ ;
-/*!50003 SET character_set_results = latin1 */ ;
-/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`rdsadmin`@`localhost`*/ /*!50003 trigger block_user_u before update on user
-for each row
-BEGIN
-  DECLARE foo varchar(255);
-  if old.User = "rdsadmin" then
-    select `ERROR (RDS): CANNOT UPDATE RDSADMIN USER` into foo from mysql.user;
-  end if;
-
-  if old.User = "rdsrepladmin" then
-    select `ERROR (RDS): CANNOT UPDATE RDSREPLADMIN USER` into foo from mysql.user;
-  end if;
-
-  if old.super_priv = 'N' and new.super_priv = 'Y' then
-     select `ERROR (RDS): SUPER PRIVILEGE CANNOT BE GRANTED OR MAINTAINED` into foo from mysql.user;
-   elseif old.shutdown_priv = 'N' and new.shutdown_priv = 'Y' then
-     select `ERROR (RDS): SHUTDOWN PRIVILEGE CANNOT BE GRANTED OR MAINTAINED. PLEASE USE RDS API` into foo from mysql.user;
-   elseif old.file_priv = 'N' and new.file_priv = 'Y' then
-     select `ERROR (RDS): FILE PRIVILEGE CANNOT BE GRANTED OR MAINTAINED` into foo from mysql.user;
-   elseif old.repl_slave_priv = 'N' and new.repl_slave_priv = 'Y' then
-     select `ERROR (RDS): REPLICA SLAVE PRIVILEGE CANNOT BE GRANTED OR MAINTAINED` into foo from mysql.user;
-   elseif old.repl_client_priv = 'N' and new.repl_client_priv = 'Y' then
-       select `ERROR (RDS): REPLICA CLIENT PRIVILEGE CANNOT BE GRANTED OR MAINTAINED` into foo from mysql.user;
-  end if;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = latin1 */ ;
-/*!50003 SET character_set_results = latin1 */ ;
-/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`rdsadmin`@`localhost`*/ /*!50003 trigger block_user_d before delete on user
-for each row
-BEGIN
-DECLARE foo varchar(255);
-if old.User = "rdsadmin" and old.Host = "localhost" then
-  select `ERROR (RDS): CANNOT DROP RDSADMIN USER` into foo;
-end if;
-if old.User = "rdsrepladmin" then
-  select `ERROR (RDS): CANNOT DROP RDSREPLADMIN USER` into foo;
-end if;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -327,4 +100,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-28 15:15:27
+-- Dump completed on 2017-10-24 17:29:02
--- sqlite3-schema.sql
+++ sqlite3-schema.sql
@@ -1,39 +1,54 @@
-CREATE TABLE IF NOT EXISTS "operations" (
-  "symbol" varchar(20) DEFAULT NULL,
-  "date" date DEFAULT NULL,
-  "type" char(1) DEFAULT NULL,
-  "size" int(11) DEFAULT NULL,
-  "price" float DEFAULT NULL,
-  "cost" float DEFAULT NULL
+CREATE TABLE `alerts` (
+  `symbol` varchar(12) DEFAULT NULL
+,  `timeframe` varchar(3) DEFAULT NULL
+,  `datetime` datetime DEFAULT NULL
+,  `alert` varchar(4) DEFAULT NULL
+,  `price` double DEFAULT NULL
+,  UNIQUE (`symbol`,`timeframe`,`datetime`,`alert`)
 );
-CREATE TABLE IF NOT EXISTS "stockinfo" (
-  "symbol" varchar(12) NOT NULL DEFAULT '',
-  "name" varchar(64) NOT NULL DEFAULT '',
-  "exchange" varchar(16) NOT NULL DEFAULT '',
-  "capitalisation" float DEFAULT NULL,
-  "low_52weeks" float DEFAULT NULL,
-  "high_52weeks" float DEFAULT NULL,
-  "earnings" float DEFAULT NULL,
-  "dividend" float DEFAULT NULL,
-  "p_e_ratio" float DEFAULT NULL,
-  "avg_volume" int(11) DEFAULT NULL,
-  "active" tinyint(1) DEFAULT '1',
-  PRIMARY KEY ("symbol")
+CREATE TABLE `intraday` (
+  `symbol` varchar(12) DEFAULT NULL
+,  `datetime` datetime DEFAULT NULL
+,  `open` double DEFAULT NULL
+,  `high` double DEFAULT NULL
+,  `low` double DEFAULT NULL
+,  `close` double DEFAULT NULL
+,  `volume` integer  NOT NULL DEFAULT '0'
+,  UNIQUE (`symbol`,`datetime`)
 );
-CREATE TABLE IF NOT EXISTS "stockprices_intraday" (
-  "symbol" varchar(12) NOT NULL DEFAULT '',
-  "datetime" datetime DEFAULT NULL,
-  "previous_close" float DEFAULT NULL,
-  "min_open" float DEFAULT NULL,
-  "min_low" float DEFAULT NULL,
-  "min_high" float DEFAULT NULL,
-  "min_close" float DEFAULT NULL,
-  "bid" float DEFAULT NULL,
-  "ask" float DEFAULT NULL,
-  "volume" int(11) DEFAULT NULL
+CREATE TABLE `operations` (
+  `symbol` varchar(20) DEFAULT NULL
+,  `date` date DEFAULT NULL
+,  `type` char(1) DEFAULT NULL
+,  `size` integer DEFAULT NULL
+,  `price` float DEFAULT NULL
+,  `cost` float DEFAULT NULL
 );
-CREATE INDEX "stockprices_intraday_stockprices_pkey" ON "stockprices_intraday" ("symbol","datetime");
-CREATE TABLE intraday (symbol varchar(12), datetime date, open double, high double, low double, close double, volume int);
-CREATE UNIQUE INDEX intradayindex on intraday(symbol, datetime);
-CREATE TABLE stockprices (symbol varchar(12), date date, previous_close double, day_open double, day_high double, day_low double, day_close double, day_change double, bid float, ask float, volume int);
-CREATE UNIQUE INDEX stockindex on stockprices(symbol, date);
+CREATE TABLE `stockinfo` (
+  `symbol` varchar(12) NOT NULL DEFAULT ''
+,  `name` varchar(64) NOT NULL DEFAULT ''
+,  `exchange` varchar(16) NOT NULL DEFAULT ''
+,  `capitalisation` float DEFAULT NULL
+,  `low_52weeks` float DEFAULT NULL
+,  `high_52weeks` float DEFAULT NULL
+,  `earnings` float DEFAULT NULL
+,  `dividend` float DEFAULT NULL
+,  `p_e_ratio` float DEFAULT NULL
+,  `avg_volume` integer DEFAULT NULL
+,  `active` integer DEFAULT '1'
+,  PRIMARY KEY (`symbol`)
+);
+CREATE TABLE `stockprices` (
+  `symbol` varchar(12) DEFAULT NULL
+,  `date` date DEFAULT NULL
+,  `previous_close` double DEFAULT NULL
+,  `day_open` double DEFAULT NULL
+,  `day_high` double DEFAULT NULL
+,  `day_low` double DEFAULT NULL
+,  `day_close` double DEFAULT NULL
+,  `day_change` double DEFAULT NULL
+,  `bid` float DEFAULT NULL
+,  `ask` float DEFAULT NULL
+,  `volume` integer DEFAULT NULL
+,  UNIQUE (`symbol`,`date`)
+);
